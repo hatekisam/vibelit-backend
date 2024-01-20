aa82ff3f9698df69971987f8693ada22aeaccff9
Mugisha Yves <mugishayves189000@gmail.com>
2024-01-20 10:13:08 +0200

    created the users crud

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Get(':id')
+  getUser(@Param('id') userId: string) {
+    return this.usersService.getUser(userId);
+  }
+
+  @Get()
+  getUsers() {
+    return this.usersService.getUsers();
+  }
+
+  @Post()
+  createUser() {
+    return this.usersService.createUser();
+  }
+
+  @Patch('id')
+  updateUser(@Param('id') userId: string) {
+    return this.usersService.updateUser(userId);
+  }
+
+  @Delete('id')
+  deleteUser(@Param('id') userId: string) {
+    return this.usersService.deleteUser(userId);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  getUser(id: string) {
+    return { msg: `Here i am one user  ${id}` };
+  }
+  getUsers() {
+    return { msg: 'Here we are users' };
+  }
+
+  createUser() {
+    return { msg: 'Guess what i was created' };
+  }
+
+  updateUser(id: string) {
+    return { msg: ` ${id} was updated` };
+  }
+
+  deleteUser(id: string) {
+    return { msg: `${id} was deleted` };
+  }
+}
